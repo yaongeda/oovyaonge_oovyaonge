a1904277f4111c3deef79f8d5831ca83ff01055c
oovyaonge <saysay123v@nate.com>
2012-05-04 15:33:00 +0900

    first commit

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
-  res.writeHead(400, {'Content-Type': 'text/plain'});
+  res.writeHead(500, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
