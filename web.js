a00ea9b5593d94073567137f8845c27411177bfc
kimsonghyun <oovyaong@gmail.com>
2018-01-11 15:51:26 +0900

    콘솔 추가 두번째

--- web.js
+++ web.js
@@ -6,3 +6,5 @@ http.createServer(function (req, res) {
 console.log('Server running at http://127.0.0.1:1337/');
 
 console.log('Server running at http://127.0.0.1:1337/');
+
+console.log('Server running at http://127.0.0.1:1337/');
